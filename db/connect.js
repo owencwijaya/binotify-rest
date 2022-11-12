314574435470da863462b6bfd9e0d9eee7de09b8
owencwijaya <owencwijaya@gmail.com>
2022-11-12 11:34:19 +0700

    chore: update ports to avoid conflict with other containers

--- db/connect.js
+++ db/connect.js
@@ -1,15 +1,9 @@
 const mysql = require("mysql");
 
-
-// MYSQL_USER: admin-rest
-// MYSQL_PASSWORD: admin-rest
-// MARIADB_ROOT_PASSWORD: MARIADB_ROOT_PASSWORD
-// MYSQL_DATABASE: binotify-rest
-
 const conn = mysql.createConnection({
-    host: 'db-binotify-rest',
-    user: 'admin',
-    password: 'admin',
+    host: 'db-binotify',
+    user: 'root',
+    password: 'root',
     database: 'binotify-rest',
 });
 
